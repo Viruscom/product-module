619f25cc4e1944ace35a0c6f5464d54c019962ee
h.chobanov <hristiqn.chobanov@gmail.com>
2023-10-03 16:56:39 +0300

    sddsf

---
+++ Resources/views/front/products/characteristics.blade.php
@@ -0,0 +1,21 @@
+<div class="prod-characteristics aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+    <h3>Characteristics</h3>
+
+    <p>
+        <strong>Nulla elit:</strong>elit quam, porta ac urna eu, eleifend commodo. elit quam, porta ac urna eu, eleifend commodo nisi. </p>
+
+    <p>
+        <strong>Nulla elit:</strong>elit quam, porta ac urna eu </p>
+
+    <p>
+        <strong>Nulla elit:</strong>elit quam </p>
+
+    <p>
+        <strong>Nulla elit:</strong>elit quam, porta </p>
+
+    <p>
+        <strong>Nulla elit:</strong>elit quam, porta ac urna eu, eleifend commodo. </p>
+
+    <p>
+        <strong>Nulla elit:</strong>elit quam, porta ac urna eu, eleifend commodo. </p>
+</div>
